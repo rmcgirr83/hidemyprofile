d9e66b14e1baf55f673f67e7b5819002dd3ff3af
Richard McGirr <rmcgirr83@gmail.com>
2020-08-25 20:47:17 -0500

    Fix phpBB validation errors

--- event/listener.php
+++ event/listener.php
@@ -91,10 +91,7 @@ class listener implements EventSubscriberInterface
 		if ($event['ext_name'] == 'rmcgirr83/hidemyprofile' && $event['action'] == 'details')
 		{
 			$this->language->add_lang('hidemyprofile', $event['ext_name']);
-			$this->template->assign_vars([
-				'L_BUY_ME_A_BEER_EXPLAIN'	=> $this->language->lang('BUY ME A BEER_EXPLAIN', '<a href="' . $this->language->lang('BUY_ME_A_BEER_URL') . '" target="_blank" rel=”noreferrer noopener”>', '</a>'),
-				'S_BUY_ME_A_BEER_HMP' => true,
-			]);
+			$this->template->assign_var('S_BUY_ME_A_BEER_HMP', true);
 		}
 	}
 
